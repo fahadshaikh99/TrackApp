af206c4cf889e16e743c2ca640fd1927b3fbb8ec
Fahad Shaikh <56800738+fahadshaikh99@users.noreply.github.com>
2020-06-13 22:47:56 +0500

    Removed unused Import section

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://e9184e1e1007.ngrok.io'
+    baseURL: '//add ngrok URL'
 });
--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -13,7 +13,6 @@ const locationReducer = (state, action) => {
     case 'change_name':
       return { ...state, name: action.payload };
     case 'reset':
-      // clean form and locations array once tracking and saving done
       return { ...state, name: '', locations: [] };
     default:
       return state;
@@ -45,60 +44,3 @@ export const { Context, Provider } = createDataContext(
   { name: '', recording: false, locations: [], currentLocation: null }
 );
 
-
-// import createDataContext from './createDataContext';
-
-// const locationReducer = (state, action) => {
-//     switch (action.type) {
-
-//         case 'add_current_location':
-//             return { ...state, currentLocation: action.payload };
-//         case 'start_recording':
-//             return { ...state, recording: true};
-//         case 'stop_recording':
-//             return { ...state, recording: false};
-//         case 'add_location':
-//             return { ...state, locations: [...state.location, action.payload] };
-
-//         case 'change_name':
-//             return { ...state, name: action.payload};
-//         default:
-//             return state;
-//     }
-// }
-
-// const changeName = dispatch => (name) => {
-//     dispatch({ type: 'change_name', payload: name });
-// }
-
-// const startRecording = dispatch => () => {
-
-//     dispatch({ type: 'start_recording'});
-
-// };
-
-
-// const stopRecording = dispatch => () => {
-
-//     dispatch({ type: 'stop_recording'});
-
-// };
-
-
-// const addLocation = dispatch => (location, recording) => {
-   
-//     dispatch({ type: 'add_current_location', payload: location});
-
-//     if(recording)
-//     {
-//         dispatch({ type: 'add_location', payload: location})
-//     }
-// };
-
-
-// export const { Context, Provider} = createDataContext(
-
-//     locationReducer,
-//     { startRecording, stopRecording, addLocation, changeName },
-//     { name: '', recording: false, location: [], currentLocation: null }
-// );
